e99eb9c010d37bbe21bd848b208b96ea26a1c64d
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-26 11:19:38 +0700

    Add files via upload

---
+++ PertidaksamaanBOOLEANnama.js
@@ -0,0 +1,8 @@
+let Boolean_Data1 = ("acep" == "acep") //true, karena acep sama dengan acep
+console.log (Boolean_Data1)
+
+let Boolean_Data2 = ("acep" == "budi") //false, karena acep bukan budi
+console.log (Boolean_Data2)
+
+let BoolNama = ("Budi" == "Marmut")
+console.log (BoolNama)
